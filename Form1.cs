3092e73e44c01162852e342baa89a36814610e83
Grzegorz <grzegorz091499@gmail.com>
2021-01-06 19:24:11 +0100

    coutdown timer

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dataFromTextbox = textBox1.Text;
+            int czas;
+            var komunikat = int.TryParse(dataFromTextbox, out czas);
+            
+            if (komunikat)
+            {
+                this.Hide();
+                Form2 f2 = new Form2(dataFromTextbox); 
+                f2.Show();
+
+            }
+
+            else
+            { 
+                label2.Text = "Podana Wartość musi być liczbowa";
+            }
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2(string czas)
+        {
+            InitializeComponent();
+            label2.Text = czas;
+            label3.Text = "false";
+        }
+
+        
+        //Start
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label3.Text = "false";
+            var time = int.Parse(label2.Text);
+            var pause = Convert.ToBoolean(label3.Text);
+            timer(time, pause);
+
+
+        }
+
+        //Stop
+
+
+        private void timer(int time, bool pause)
+        {
+            while (time > 0 && pause == false)
+            {
+                Thread.Sleep(1000);
+                time--;
+                updateTime(time);
+                label2.Text = time.ToString();
+                label2.Refresh();
+                if (Convert.ToBoolean(label3.Text) == true)
+                {
+                    pause = true;
+                }
+            }
+            timer(time, pause);
+        }
+
+        private void updateTime(int time)
+        {
+            int minuty = time / 60;
+            int sekundy = time % 60;
+            var labelText = "Pozostało " + minuty.ToString() + " minut " + sekundy.ToString() + " sekund ";
+            //MessageBox.Show(labelText, "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            label1.Text = String.Format(labelText);
+            label1.Refresh();
+        }
+
+
+    }
+}
